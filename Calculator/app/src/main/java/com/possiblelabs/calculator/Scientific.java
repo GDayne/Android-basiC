989f6f46ed3b7983432cff596222625d307a5d53
GDayne <guerra.dayne@gmail.com>
2015-06-30 21:43:51 -0400

    implementando

---
+++ Calculator/app/src/main/java/com/possiblelabs/calculator/Scientific.java
@@ -0,0 +1,19 @@
+package com.possiblelabs.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Saul on 30/06/2015.
+ */
+public class Scientific extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scientific);
+
+
+    }
+}
